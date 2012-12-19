d3e896eb48b4926439acda4f63f93a5805e1426e
Arpad Rimmel <arpad.rimmel@free.fr>
2012-12-19 10:55:05 +0100

    delete useless fils

--- latinSquare/cpp/randLSfinder.h
+++
@@ -1,9 +0,0 @@
-#include "LSfinder.h"
-
-class randLSfinder: public LSfinder {
-
-    public:
-    randLSfinder(int dim, int size) : LSfinder(dim,size) {};
-    LS* findLS(int nbTry=100, double time=0);
-
-};
